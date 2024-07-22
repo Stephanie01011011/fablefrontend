b70a7e145afac4501a8c6db218a98c1d7a8714a3
Stephanie Livengood <sreneelive@gmail.com>
2024-07-22 13:49:39 -0400

    fix book info routing

--- src/components/GenreBookDisplay.js
+++ src/components/GenreBookDisplay.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const GenreBookDisplay = ({book}) => {
     let address = book._id + ".jpg"
@@ -10,7 +11,7 @@ const GenreBookDisplay = ({book}) => {
       <h2>{book.title}</h2>
       <p className="body-text dark">{book.author}</p>
       <p className="card-text">{book.price}</p>
-      <a href={url} className='btn'>View Book</a>
+      <Link to={url} className='btn'>View Book</Link>
     </div>
   )
 }
